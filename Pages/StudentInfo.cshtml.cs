a97578347160412ece639dff9d799a5e0c5a3aab
djr59 <enginenear123@gmail.com>
2023-05-08 05:03:55 -0400

    massive overhaul to the UI + Search and match Capabilities of Angular App. gave this all of the features of the Angular app but included some animations and fun stuff.
    I could break this into two commits or three even, to distribute the changes to be one commit per ticket/change. This is how i have learned and prefer to do commits in a professional environment, and i would have done this in that way if this was not just my own little project where i am trying to get the code done with and the integrity of the commits and what we are pushing up is trivial.
    
    ui updates, bootstrap, added all search and features to match that of the angular project capabilities.

--- Pages/StudentInfo.cshtml.cs
+++ Pages/StudentInfo.cshtml.cs
@@ -10,6 +10,11 @@ namespace ASP_NET_Simulation.Pages
     public class StudentInfoModel : PageModel
     {
         private readonly StudentService _studentService;
+        public string SearchQuery { get; set; }
+        public string FinancialField { get; set; }
+        public string ComparisonType { get; set; }
+        public decimal? FinancialValue { get; set; }
+
 
         public StudentInfoModel(StudentService studentService)
         {
@@ -18,9 +23,49 @@ namespace ASP_NET_Simulation.Pages
 
         public List<Student> Students { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchQuery, string financialField, string comparisonType, decimal? financialValue)
         {
-            Students = await _studentService.GetStudentsAsync();
+            SearchQuery = searchQuery;
+            FinancialField = financialField;
+            ComparisonType = comparisonType;
+            FinancialValue = financialValue;
+
+            var students = await _studentService.GetStudentsAsync();
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                students = students.Where(s => s.Name.IndexOf(searchQuery, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(financialField) && !string.IsNullOrEmpty(comparisonType) && financialValue.HasValue)
+            {
+                students = students.Where(s => FilterByFinancialCondition(s, financialField, comparisonType, financialValue.Value)).ToList();
+            }
+
+            Students = students;
         }
+
+        private bool FilterByFinancialCondition(Student student, string financialField, string comparisonType, decimal value)
+        {
+            decimal fieldValue = financialField switch
+            {
+                "balance" => student.Balance,
+                "financialAid" => student.FinancialAid,
+                "tuition" => student.Tuition,
+                "fees" => student.Fees,
+                "scholarships" => student.Scholarships,
+                _ => throw new ArgumentException("Invalid financial field")
+            };
+
+            return comparisonType switch
+            {
+                "lessThan" => fieldValue < value,
+                "equalTo" => fieldValue == value,
+                "greaterThan" => fieldValue > value,
+                _ => throw new ArgumentException("Invalid comparison type")
+            };
+        }
+
+
     }
 }
